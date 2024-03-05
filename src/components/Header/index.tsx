b83c9f11042de145ee8e903ea05cd6e7c928ccc7
MatheusPalmieri <matheuspalmieridev@gmail.com>
2024-03-05 11:14:42 -0300

    feat: remove search

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,10 @@
 import { Logo } from '../Logo';
-// import { Search } from '../Search';
 import styles from './style.module.css';
 
 export const Header = () => {
   return (
-    <header className={styles.head}>
-      <div className={`container ${styles.header}`}>
-        <Logo />
-        {/* <Search /> */}
-      </div>
+    <header className={styles.header}>
+      <Logo />
     </header>
   );
 };
